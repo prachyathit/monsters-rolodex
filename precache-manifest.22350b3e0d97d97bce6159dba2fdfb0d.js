b4079eb24e43f953b3cc2842f7360e1ee662e286
Prachyathit Kanburapa <prachyathit.k@gmail.com>
2019-07-11 15:14:46 +0700

    Updates

---
+++ precache-manifest.22350b3e0d97d97bce6159dba2fdfb0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0097cc278b67860a8e173c35e795bcfb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5d077ee2ac7c5c50419f",
+    "url": "/monsters-rolodex/static/css/main.9961d906.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5d077ee2ac7c5c50419f",
+    "url": "/monsters-rolodex/static/js/main.17533ad6.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
